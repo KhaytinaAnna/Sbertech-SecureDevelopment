698cf4f2754139d935a087f44aa7520fab03b13d
KhaytinaAnna <71896956+KhaytinaAnna@users.noreply.github.com>
2025-04-15 16:13:44 +0300

    Update checksums.sh

--- checksums.sh
+++ checksums.sh
@@ -2,7 +2,6 @@
 
 CHECKSUM_FILE="dlib_checksums.sha256"
 
-# Использование find и sha256sum для создания контрольных сумм
 find ./dlib -type f -print0 | while IFS= read -r -d $'\0' file; do
     sha256sum "$file" >> "$CHECKSUM_FILE"
 done
